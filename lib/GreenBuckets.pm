91fdda3dd4f5c3d5258e8a38d87917a9ef7f2897
Masahiro Nagano <kazeburo@gmail.com>
2011-04-19 18:50:07 +0900

    iroiro

--- lib/GreenBuckets.pm
+++ lib/GreenBuckets.pm
@@ -2,9 +2,41 @@ package GreenBuckets;
 
 use strict;
 use warnings;
+use 5.12;
 
 our $VERSION = 0.01;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+GreenBuckets - simple object storage
+
+=head1 SYNOPSIS
+
+  ...
+
+=head1 DESCRIPTION
+
+GreenBuckets is simple object storage system, ordinaly used for mass image storage
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 
